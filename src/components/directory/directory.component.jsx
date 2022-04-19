14b7a5eb864cc74a806c2de0c1ad38b936ee5ede
Owen Baldwin <owenabaldwin@gmail.com>
2022-04-19 14:41:40 +0100

    basic app created, nav and home comps and routes done, directory and diritem comps created and working

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,54 @@
+import DirectoryItem from "../directory-item/directory-item.component";
+
+const categories = [
+    {
+      id: 1,
+      title: 'FilmPod',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      // route: 'shop/hats'
+    },
+    {
+      id: 2,
+      title: 'Copson Studio Wiring',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      // route: 'shop/jackets'
+    },
+    {
+      id: 3,
+      title: 'Owen Baldwin - Film Composer',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      // route: 'shop/sneakers'
+    },
+    {
+      id: 4,
+      title: 'Watchlist',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      // route: 'shop/womens'
+    },
+    {
+      id: 5,
+      title: 'Demo React Shop',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      // route: 'shop/mens'
+    },
+    {
+      id: 6,
+      title: 'IsleBnB',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      // route: 'shop/mens'
+    },
+  ];
+
+
+const Directory = () => {
+  return (
+    <div>
+      {categories.map((category) => (
+        <DirectoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  )
+
+};
+
+export default Directory;
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+
+
+const Navigation = () => {
+  return (
+    <div>
+      <h3>Nagivatey navigatah</h3>
+      <Outlet />
+    </div>
+  )
+};
+
+export default Navigation;
