73df2e8345ae422aabb5a6a91f14f3537dbaa523
Owen Baldwin <owenabaldwin@gmail.com>
2022-04-20 15:42:35 +0100

    category comp displaying content based on params

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,7 +11,7 @@ const categories = [
       id: 2,
       title: 'Copson Studio Wiring',
       imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-      route: '/copson-studio-wiring'
+      route: '/copsonstudiowiring'
     },
     {
       id: 3,
---
+++ src/portfolio-data.js
@@ -0,0 +1,35 @@
+const PORTFOLIO_DATA = [
+  {
+    id: 8,
+    title: 'filmpod',
+    items: [
+      {
+        id: 11,
+        presentation: 'App for film prods',
+      },
+      {
+        id: 12,
+        extlink: 'filmpod Site link goes here',
+      },
+
+    ],
+  },
+  {
+    id: 9,
+    title: 'copsonstudiowiring',
+    items: [
+      {
+        id: 16,
+        presentation: 'Sean s website',
+      },
+      {
+        id: 17,
+        extlink: 'CSW Site link goes here',
+      },
+
+    ],
+  },
+
+]
+
+export default PORTFOLIO_DATA;
