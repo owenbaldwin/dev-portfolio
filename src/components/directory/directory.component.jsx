9814746c8a821b1ecb6eabf30468c4ff41952e6c
Owen Baldwin <owenabaldwin@gmail.com>
2022-04-25 16:55:59 +0100

    home css done

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,40 +1,41 @@
 import DirectoryItem from "../directory-item/directory-item.component";
+import './directory.styles.scss';
 
 const categories = [
     {
       id: 1,
       title: 'FilmPod',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      imageUrl: 'https://i.ibb.co/TbyrPqf/Screenshot-2021-12-08-at-14-51-58.png',
       route: '/filmpod'
     },
     {
       id: 2,
       title: 'Copson Studio Wiring',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      imageUrl: 'https://i.ibb.co/3sCZJ58/csw-home.png',
       route: '/copsonstudiowiring'
     },
     {
       id: 3,
       title: 'Owen Baldwin - Film Composer',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      imageUrl: 'https://i.ibb.co/r21z6cN/ob-home.png',
       route: '/owen-baldwin'
     },
     {
       id: 4,
       title: 'Watchlist',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      imageUrl: 'https://i.ibb.co/56s28Rr/wl-home.png',
       route: '/watchlist'
     },
     {
       id: 5,
       title: 'Demo React Shop',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl: 'https://i.ibb.co/4pVXb9H/crwn-home.png',
       route: 'demo-react-shop'
     },
     {
       id: 6,
       title: 'IsleBnB',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl: 'https://i.ibb.co/LnR5nsN/islebnb-landingpage.png',
       route: '/islebnb'
     },
   ];
@@ -42,7 +43,12 @@ const categories = [
 
 const Directory = () => {
   return (
-    <div>
+    <div className="directory-container">
+      <div className="directory-title">
+        <div className="separator" />
+        <h2>Portfolio</h2>
+        <div className="separator" />
+      </div>
       {categories.map((category) => (
         <DirectoryItem key={category.id} category={category} />
       ))}
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,22 @@
 import { Outlet } from 'react-router-dom';
-
+import './navigation.styles.scss';
 
 const Navigation = () => {
   return (
-    <div>
-      <h3>Nagivatey navigatah</h3>
+    <div className='navigation-container'>
+      <div className='navbar-container'>
+        <div className='navbar-inner'>
+          <div className='nav-left'>
+            <h1>Owen</h1>
+            <h1>Baldwin</h1>
+          </div>
+          <div className='nav-right'>
+            <h3>Home</h3>
+            <h3>About</h3>
+            <h3>Contact</h3>
+          </div>
+        </div>
+      </div>
       <Outlet />
     </div>
   )
