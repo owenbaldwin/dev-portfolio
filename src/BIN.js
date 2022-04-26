f94219fb0c3be2f3e947a4af045f41705eaae441
Owen Baldwin <owenabaldwin@gmail.com>
2022-04-26 18:19:45 +0100

    full wb db done in json

--- src/BIN.js
+++ src/BIN.js
@@ -37,3 +37,100 @@
 
 
               <div className='background-image' style={{backgroundImage: `url(${object.items[2].imageUrl})`}}/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+{
+    id: 3,
+    title: 'owen-baldwin',
+    items: [
+      {
+        id: 31,
+        presentation: "This is a website for my services as a film music composer, to showcase my portfolio and display contact information. The website was built with an Art Deco aesthetic, in link with the early days of cinema. Click on the link below to see the full website!",
+      },
+      {
+        id: 32,
+        tech: "Built using Ruby on Rails, HTML, CSS, GitHub, Heroku",
+      },
+      {
+        id: 33,
+        wblink: 'OB Site link goes here',
+      },
+      {
+        id: 34,
+        ghlink: 'GH repo link goes here',
+      },
+      {
+        id: 35,
+        images: [
+          {
+            id: 36,
+            imageUrl: 'https://i.ibb.co/r21z6cN/ob-home.png',
+            imageDescription: 'Website landing page',
+          },
+          {
+            id: 37,
+            imageUrl: 'https://i.ibb.co/bHH81vG/ob-portfolio.png',
+            imageDescription: 'Section of the portfolio page',
+          },
+          {
+            id: 38,
+            imageUrl: 'https://i.ibb.co/ThZJRX2/ob-refs.png',
+            imageDescription: 'References page',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: 'owen-baldwin',
+    items: [
+      {
+        id: 39,
+        presentation: "This is a website for my services as a film music composer, to showcase my portfolio and display contact information. The website was built with an Art Deco aesthetic, in link with the early days of cinema. Click on the link below to see the full website!",
+      },
+      {
+        id: 40,
+        tech: "Built using Ruby on Rails, HTML, CSS, GitHub, Heroku",
+      },
+      {
+        id: 41,
+        wblink: 'OB Site link goes here',
+      },
+      {
+        id: 42,
+        ghlink: 'GH repo link goes here',
+      },
+      {
+        id: 43,
+        images: [
+          {
+            id: 44,
+            imageUrl: 'https://i.ibb.co/r21z6cN/ob-home.png',
+            imageDescription: 'Website landing page',
+          },
+          {
+            id: 45,
+            imageUrl: 'https://i.ibb.co/bHH81vG/ob-portfolio.png',
+            imageDescription: 'Section of the portfolio page',
+          },
+          {
+            id: 46,
+            imageUrl: 'https://i.ibb.co/ThZJRX2/ob-refs.png',
+            imageDescription: 'References page',
+          },
+        ],
+      },
+    ],
+  },
--- src/portfolio-data.js
+++ src/portfolio-data.js
@@ -9,49 +9,53 @@ const PORTFOLIO_DATA = [
       },
       {
         id: 12,
-        wblink: 'filmpod Site link goes here',
+        tech: "Built using Ruby on Rails, HTML, CSS, Javascript, GitHub, Heroku.",
       },
       {
         id: 13,
-        ghlink: 'GH repo link goes here',
+        wblink: 'filmpod Site link goes here',
       },
       {
         id: 14,
+        ghlink: 'GH repo link goes here',
+      },
+      {
+        id: 15,
         images: [
           {
-            id: 15,
-            imageUrl: 'https://i.ibb.co/TbyrPqf/Screenshot-2021-12-08-at-14-51-58.png',
-            imageDescription: 'FilmPod landing Page',
-          },
-          {
             id: 16,
-            imageUrl: 'https://i.ibb.co/0MyLDtN/filmpod-home.png',
-            imageDescription: 'FilmPod home Page',
+            imageUrl: 'https://i.ibb.co/TbyrPqf/Screenshot-2021-12-08-at-14-51-58.png',
+            imageDescription: 'FilmPod landing Page.',
           },
           {
             id: 17,
-            imageUrl: 'https://i.ibb.co/Rg9QG81/filmpod-dpts.png',
-            imageDescription: 'From The Corner Of My Eye selected with each current departments displayed',
+            imageUrl: 'https://i.ibb.co/0MyLDtN/filmpod-home.png',
+            imageDescription: 'FilmPod home Page.',
           },
           {
             id: 18,
-            imageUrl: 'https://i.ibb.co/ZdvKdxS/filmpod-crew.png',
-            imageDescription: 'The user can add new members to the team and assign them a department and rank',
+            imageUrl: 'https://i.ibb.co/Rg9QG81/filmpod-dpts.png',
+            imageDescription: 'From The Corner Of My Eye selected with each current departments displayed.',
           },
           {
             id: 19,
-            imageUrl: 'https://i.ibb.co/MyvrXbb/filmpod-acting.png',
-            imageDescription: 'Acting department selected, showing the different tasks and their status',
+            imageUrl: 'https://i.ibb.co/ZdvKdxS/filmpod-crew.png',
+            imageDescription: 'The user can add new members to the team and assign them a department and rank.',
           },
           {
             id: 20,
-            imageUrl: 'https://i.ibb.co/z7jsMTg/filmpod-submission.png',
-            imageDescription: "Checking out Leo's submission and approving it",
+            imageUrl: 'https://i.ibb.co/MyvrXbb/filmpod-acting.png',
+            imageDescription: 'Acting department selected, showing the different tasks and their status.',
           },
           {
             id: 21,
+            imageUrl: 'https://i.ibb.co/z7jsMTg/filmpod-submission.png',
+            imageDescription: "Checking out Leo's submission and approving it.",
+          },
+          {
+            id: 22,
             imageUrl: 'https://i.ibb.co/VgZXN0n/filmpod-moodboard.png',
-            imageDescription: "Checking out the acting mood board where Leo's photo has been added upon submission approval",
+            imageDescription: "Checking out the acting mood board where Leo's photo has been added upon submission approval.",
           },
         ],
       },
@@ -62,17 +66,226 @@ const PORTFOLIO_DATA = [
     title: 'copsonstudiowiring',
     items: [
       {
-        id: 16,
-        presentation: 'Sean s website',
+        id: 23,
+        presentation: "This is a website I built for a friend's business that specialises in studio wiring and building.",
       },
       {
-        id: 17,
-        extlink: 'CSW Site link goes here',
+        id: 24,
+        tech: "Built using Ruby on Rails, HTML, CSS, GitHub, Heroku.",
       },
-
+      {
+        id: 25,
+        wblink: 'CSW Site link goes here.',
+      },
+      {
+        id: 26,
+        ghlink: 'GH repo link goes here.',
+      },
+      {
+        id: 27,
+        images: [
+          {
+            id: 28,
+            imageUrl: 'https://i.ibb.co/3sCZJ58/csw-home.png',
+            imageDescription: 'Copson Studio Wiring landing page.',
+          },
+          {
+            id: 29,
+            imageUrl: 'https://i.ibb.co/ySDrtRz/csw-gallery.png',
+            imageDescription: 'Gallery section.',
+          },
+          {
+            id: 30,
+            imageUrl: 'https://i.ibb.co/dJ4WvmK/csw-rates.png',
+            imageDescription: 'Rates and contact sections.',
+          },
+        ],
+      },
     ],
   },
-
+  {
+    id: 3,
+    title: 'owen-baldwin',
+    items: [
+      {
+        id: 31,
+        presentation: "This is a website for my services as a film music composer, to showcase my portfolio and display contact information. The website was built with an Art Deco aesthetic, in link with the early days of cinema. Click on the link below to see the full website!",
+      },
+      {
+        id: 32,
+        tech: "Built using Ruby on Rails, HTML, CSS, GitHub, Heroku.",
+      },
+      {
+        id: 33,
+        wblink: 'OB Site link goes here',
+      },
+      {
+        id: 34,
+        ghlink: 'GH repo link goes here',
+      },
+      {
+        id: 35,
+        images: [
+          {
+            id: 36,
+            imageUrl: 'https://i.ibb.co/r21z6cN/ob-home.png',
+            imageDescription: "Website's landing page.",
+          },
+          {
+            id: 37,
+            imageUrl: 'https://i.ibb.co/bHH81vG/ob-portfolio.png',
+            imageDescription: 'Section of the portfolio page.',
+          },
+          {
+            id: 38,
+            imageUrl: 'https://i.ibb.co/ThZJRX2/ob-refs.png',
+            imageDescription: 'References page.',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: 'watchlist',
+    items: [
+      {
+        id: 39,
+        presentation: "This is a web app I am buidling that allows the user to create lists of films. This project is still in progress. I have written Javascript code that takes a user input from a searchbar and scrapes film information from IMDb. Unfortunately I have not yet had the time to implement this code in this project as I have been very busy with other project and job hunting. I have learned React in the meantime and I'm now considering using it for the front end as an exercise to join two frameworks together. This web app was done in a Googie style, in the theme of 1950s drive-in cinemas. Click on the link below to see the full website!",
+      },
+      {
+        id: 40,
+        tech: "Built using Ruby on Rails, HTML, CSS, Javascript, GitHub, Heroku.",
+      },
+      {
+        id: 41,
+        wblink: 'WL Site link goes here',
+      },
+      {
+        id: 42,
+        ghlink: 'GH repo link goes here',
+      },
+      {
+        id: 43,
+        images: [
+          {
+            id: 44,
+            imageUrl: 'https://i.ibb.co/56s28Rr/wl-home.png',
+            imageDescription: "Website's landing page.",
+          },
+          {
+            id: 45,
+            imageUrl: 'https://i.ibb.co/RhfGHGP/wl-good.png',
+            imageDescription: 'A list of films with buttons on top.',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: 'demo-react-shop',
+    items: [
+      {
+        id: 46,
+        presentation: "This is a web app I built to learn React, for a fictional online store. It uses Firebase for the user and product databases. I am in the early stages of building an actual online store for an independant clothing brand. This served as an initial testing ground to learn. Click on the link below to see the full website!",
+      },
+      {
+        id: 47,
+        tech: "Built using React, Javascript, HTML, CSS, GitHub, Netlify, Firebase.",
+      },
+      {
+        id: 48,
+        wblink: 'DRS Site link goes here',
+      },
+      {
+        id: 49,
+        ghlink: 'GH repo link goes here',
+      },
+      {
+        id: 50,
+        images: [
+          {
+            id: 51,
+            imageUrl: 'https://i.ibb.co/4pVXb9H/crwn-home.png',
+            imageDescription: "Website's landing page.",
+          },
+          {
+            id: 52,
+            imageUrl: 'https://i.ibb.co/HpdCmjM/crwn-items.png',
+            imageDescription: 'Hats section of the store.',
+          },
+          {
+            id: 53,
+            imageUrl: 'https://i.ibb.co/bHrD47S/crwn-login.png',
+            imageDescription: 'Login page with items in the cart in the top right-hand corner.',
+          },
+          {
+            id: 54,
+            imageUrl: 'https://i.ibb.co/P4RLzbG/crwn-checkout.png',
+            imageDescription: 'Cart items displayed.',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: 'islebnb',
+    items: [
+      {
+        id: 55,
+        presentation: "This is a clone of AirBnB where users can either rent islands or become hosts. This was built with three other junior developers as part of Le Wagon's curriculum. Click on the link below to see the full website!",
+      },
+      {
+        id: 56,
+        tech: "Built using Ruby on Rails, Javascript, HTML, CSS, GitHub, Heroku.",
+      },
+      {
+        id: 57,
+        wblink: 'ib Site link goes here',
+      },
+      {
+        id: 58,
+        ghlink: 'GH repo link goes here',
+      },
+      {
+        id: 59,
+        images: [
+          {
+            id: 60,
+            imageUrl: 'https://i.ibb.co/LnR5nsN/islebnb-landingpage.png',
+            imageDescription: "Website's landing page.",
+          },
+          {
+            id: 61,
+            imageUrl: 'https://i.ibb.co/vXGJVzp/islebnb-login.png',
+            imageDescription: 'Login page.',
+          },
+          {
+            id: 62,
+            imageUrl: 'https://i.ibb.co/mhF9C8m/islebnb-browse.png',
+            imageDescription: 'List of islands for rent.',
+          },
+          {
+            id: 63,
+            imageUrl: 'https://i.ibb.co/whHLMcB/islebnb-booking.png',
+            imageDescription: 'Additional info for Exuma.',
+          },
+          {
+            id: 64,
+            imageUrl: 'https://i.ibb.co/TKMJZWr/islebnb-dates.png',
+            imageDescription: 'Booking page.',
+          },
+          {
+            id: 65,
+            imageUrl: 'https://i.ibb.co/sjYZvb4/islebnb-host.png',
+            imageDescription: 'Become a host page.',
+          },
+        ],
+      },
+    ],
+  },
 ]
 
 export default PORTFOLIO_DATA;
